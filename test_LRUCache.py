74704cfe41962dc9c0bf78f34c227dee01d3db06
imjonlam <jonathanlam.1994@gmail.com>
2021-02-23 14:28:43 -0500

    updated docstrings, added more tests

--- test_LRUCache.py
+++ test_LRUCache.py
@@ -42,12 +42,12 @@ def supply_one_item_cache():
 ##########################
 
 def test_negative_capacity_initialization():
-  '''the maximum cache size must be greater than or equal to 0'''
+  '''EXCEPTIONS: the maximum cache size must be greater than or equal to 0'''
   with pytest.raises(ValueError):
     LRUCache(0)
 
 def test_getting_unknown_key(supply_sample_cache):
-  '''the key must exist when trying to retrieve'''
+  '''EXCEPTIONS: the key must exist when trying to retrieve'''
   with pytest.raises(KeyError):
     supply_sample_cache.get('UNKNOWN_KEY')
 
@@ -56,13 +56,13 @@ def test_getting_unknown_key(supply_sample_cache):
 ##########################
 
 def test_get_bring_to_front(supply_sample_cache):
-  '''retrieving a key should bring it to the front'''
+  '''GET: retrieving a key should bring it to the front'''
   val = supply_sample_cache.get('three')
   assert(val == 3)
   assert(supply_sample_cache.head.val == val)
 
 def test_get_most_used(supply_sample_cache):
-  '''retrieving most-used node; should not change cache order'''
+  '''GET: retrieving most-used node; should not change cache order'''
   head_val = supply_sample_cache.head.val
 
   val = supply_sample_cache.get('five')
@@ -70,7 +70,7 @@ def test_get_most_used(supply_sample_cache):
 
 
 def test_get_least_used(supply_sample_cache):
-  '''retrieving least-used item, new tail should become second last'''
+  '''GET: retrieving least-used item, new tail should become second last'''
   head_val = supply_sample_cache.head.val
   tail_val = supply_sample_cache.tail.val
   tail_prev_val = supply_sample_cache.tail.prev.val
@@ -85,7 +85,7 @@ def test_get_least_used(supply_sample_cache):
 ##########################
 
 def test_put_into_empty_cache(supply_empty_cache):
-  '''put new node into empty cache'''
+  '''PUT: put new node into empty cache'''
   assert(supply_empty_cache.count == 0)
 
   supply_empty_cache.put('one', 1)
@@ -96,7 +96,7 @@ def test_put_into_empty_cache(supply_empty_cache):
   assert(supply_empty_cache.items.get('one', None) is not None)
 
 def test_put_to_front(supply_sample_cache):
-  '''put new node into existing cache; update most used'''
+  '''PUT: put new node into existing cache; update most used'''
   head_val = supply_sample_cache.head.val
   tail_val = supply_sample_cache.tail.val
 
@@ -107,7 +107,7 @@ def test_put_to_front(supply_sample_cache):
   assert(supply_sample_cache.items.get('six', None) is not None)
 
 def test_put_overflow1(supply_sample_cache):
-  '''test overflow of cache of capacity - removes least-used node before new put'''
+  '''PUT: test overflow of cache of capacity - removes least-used node before new put'''
   supply_sample_cache.put('six', 6)
   initial_count = supply_sample_cache.count
   tail_prev_val = supply_sample_cache.tail.prev.val
@@ -119,7 +119,7 @@ def test_put_overflow1(supply_sample_cache):
   assert(supply_sample_cache.items.get('one', None) is None)
 
 def test_put_overflow2(supply_one_item_cache):
-  '''test overflow of a cache with capacity of 1'''
+  '''PUT: test overflow of a cache with capacity of 1'''
   supply_one_item_cache.put('two', 2)
   assert(supply_one_item_cache.count == 1)
   assert(supply_one_item_cache.head == supply_one_item_cache.tail)
@@ -127,7 +127,7 @@ def test_put_overflow2(supply_one_item_cache):
   assert(supply_one_item_cache.items.get('two', None) is not None)
 
 def test_put_existing_key1(supply_sample_cache):
-  '''inserting an existing key - updates value, puts to front'''
+  '''PUT: inserting an existing key - updates value, puts to front'''
   initial_count = supply_sample_cache.count
 
   supply_sample_cache.put('two', 20)
@@ -136,7 +136,7 @@ def test_put_existing_key1(supply_sample_cache):
   assert(supply_sample_cache.head.val == 20)
 
 def test_put_existing_key2(supply_sample_cache):
-  '''inserting an existing key into least used item - updates value, puts to front'''
+  '''PUT: inserting an existing key corresponding to least used item - updates value, puts to front'''
   initial_count = supply_sample_cache.count
   tail_prev_val = supply_sample_cache.tail.prev
 
@@ -146,12 +146,22 @@ def test_put_existing_key2(supply_sample_cache):
   assert(supply_sample_cache.head.val == 10)
   assert(supply_sample_cache.tail.val == tail_prev_val.val)
 
+def test_put_existing_key3(supply_sample_cache):
+  '''PUT: inserting an existing key corresponding to most used item - updates value'''
+  initial_count = supply_sample_cache.count
+  initial_head = supply_sample_cache.head
+
+  supply_sample_cache.put('five', 50)
+  assert(supply_sample_cache.count == initial_count)
+  assert(supply_sample_cache.head.key == initial_head.key)
+  assert(supply_sample_cache.head.val == 50)
+
 # ##########################
 # # Testing for DELETE     #
 # ##########################
 
 def test_delete_head(supply_sample_cache):
-  '''delete head node, test count decrement'''
+  '''DELETE: delete head node, test count decrement'''
   initial_count = supply_sample_cache.count
   next_val = supply_sample_cache.head.next.val
   supply_sample_cache.delete('five')
@@ -160,14 +170,14 @@ def test_delete_head(supply_sample_cache):
   assert(supply_sample_cache.items.get('five', None) is None)
 
 def test_delete_tail(supply_sample_cache):
-  '''delete from tail, new least used should be previously second last'''
+  '''DELETE: delete from tail, new least used should be previously second last'''
   tail_prev_val = supply_sample_cache.tail.prev.val
   supply_sample_cache.delete('one')
   assert(supply_sample_cache.tail.val == tail_prev_val)
   assert(supply_sample_cache.items.get('one', None) is None)
 
 def test_delete_only_remaining(supply_one_item_cache):
-  '''delete the last item in cache; resets'''
+  '''DELETE: delete the last item in cache; resets'''
   supply_one_item_cache.delete('one')
   assert(supply_one_item_cache.head is None)
   assert(supply_one_item_cache.tail is None)
@@ -175,7 +185,7 @@ def test_delete_only_remaining(supply_one_item_cache):
   assert(not supply_one_item_cache.items)
 
 def test_delete_unknown_key1(supply_empty_cache):
-  '''attempt to delete a key from an empty cache'''
+  '''DELETE: attempt to delete a key from an empty cache'''
   supply_empty_cache.delete('unknown_key')
   assert(supply_empty_cache.head is None)
   assert(supply_empty_cache.tail is None)
@@ -183,7 +193,7 @@ def test_delete_unknown_key1(supply_empty_cache):
   assert(not supply_empty_cache.items)
 
 def test_delete_unknown_key2(supply_one_item_cache):
-  '''attempt to delete a key from an non-empty cache'''
+  '''DELETE: attempt to delete a key from an non-empty cache'''
   supply_one_item_cache.delete('unknown_key')
   assert(supply_one_item_cache.count == 1)
   assert(supply_one_item_cache.head == supply_one_item_cache.tail)
@@ -194,7 +204,7 @@ def test_delete_unknown_key2(supply_one_item_cache):
 # ##########################
 
 def test_reset(supply_sample_cache):
-  '''test resetting cache'''
+  '''RESET: test resetting cache'''
   supply_sample_cache.reset()
   assert(supply_sample_cache.head is None)
   assert(supply_sample_cache.tail is None)
